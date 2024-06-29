60e1139b839057431ff7ad8f3e990d80365e61e4
Juan <juanmisiac@gmail.com>
2024-06-29 19:57:20 -0300

    Diseño responsive de SideNavBar completado

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -8,27 +8,27 @@ import { Link } from 'react-router-dom';
 //Hay que hacer un estilo especifico para el boton del sidebar que es el link actual
 
 export const SideNavBar = (/*props: Props*/) => {
-  const [expanded, setExpanded] = useState(true) //EN EL FUTURO MEJORAR EL ESTADO DE expanded, NECESITO QUE SEA ESTADO GLOBAL
+  const [expanded, setExpanded] = useState(false) //EN EL FUTURO MEJORAR EL ESTADO DE expanded, NECESITO QUE SEA ESTADO GLOBAL
   return (
-    <aside className={`h-screen ${expanded ? "w-72" : "w-18"} duration-150 shadow-lg`}>
-      <nav className='h-full flex flex-col bg-blue-200 dark:bg-blue-950'>
+    <aside className={`h-screen ${expanded ? "w-72" : "w-18"} duration-150 sm:shadow-lg z-30`}>
+      <nav className={`h-full flex flex-col sm:bg-blue-200/80 sm:dark:bg-blue-950/80 ${expanded ? "bg-blue-200/80 dark:bg-blue-950/80 backdrop-blur-sm" : ""}`}>
         <div className='p-4 pb-2 flex justify-between items-center text-blue-400 dark:text-white font-medium'>
           <h2 className='text-2xl'>{expanded ? `Monedas` : ``}</h2>
           <button
-            className='p-1.5 rounded-lg bg-blue-400 dark:bg-blue-900 duration-150 hover:scale-105'
+            className='p-1.5 rounded-lg bg-blue-400 dark:bg-blue-900 duration-150 hover:scale-105 shadow-xl'
             onClick={() => setExpanded(curr => !curr)}>
             {expanded ? <ArrowLeftFromLine className='text-white' /> : <ArrowRightFromLine className='text-white' />}
           </button>
         </div>
-        <div className='flex flex-col px-4'>
+        <div className={`${expanded ? "" : "hidden"} sm:flex px-4`}>
           <ul className='text-white dark:text-white font-medium text-xl'>
             <li className='mt-4'>
-              <button className={`bg-blue-400 dark:bg-blue-900 px-2 py-1 rounded-lg duration-150 hover:scale-105 ${expanded ? "" : "w-9"}`}>
+              <button className={`bg-blue-400 dark:bg-blue-900 px-2 py-1 rounded-lg duration-150 hover:scale-105 shadow-xl ${expanded ? "" : "w-9"}`}>
                 <Link to={"/dolares"} className={`${expanded ? "" : "text-xs flex justify-center py-1.5"}`}>{expanded ? `Dolares` : `USD`}</Link>
               </button>
             </li>
             <li className='mt-3'>
-              <button className={`bg-blue-400 dark:bg-blue-900 px-2 py-1 rounded-lg duration-150 hover:scale-105 ${expanded ? "" : "w-9"}`}>
+              <button className={`bg-blue-400 dark:bg-blue-900 px-2 py-1 rounded-lg duration-150 hover:scale-105 shadow-xl ${expanded ? "" : "w-9"}`}>
                 <Link to={"/euros"} className={`${expanded ? "" : "text-xs flex justify-center py-1.5"}`}>{expanded ? `Euros` : `EUR`}</Link>
               </button>
             </li>
--- src/routes/DolaresPage.tsx
+++ src/routes/DolaresPage.tsx
@@ -7,7 +7,7 @@ export const DolaresPage = () => {
       <Header />
       <div className="flex bg-blue-100 dark:bg-[#1f305c]">
         <SideNavBar></SideNavBar>
-        <div className="flex mt-6 ms-12 text-blue-400 dark:text-white">
+        <div className="absolute sm:relative flex mt-16 sm:mt-6 ms-16 sm:m-12 text-blue-400 dark:text-white">
           <h2 className="text-3xl font-bold">Dolares</h2>
         </div>
       </div>
